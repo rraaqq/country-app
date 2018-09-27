403a735a0c192de330743596574e2e0b1d092087
Michal Raczkowski <michal.raczkowski.88@gmail.com>
2018-09-27 21:20:12 +0100

    Add Redux part.2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './DevTools';
+import routes from './routes';
+//import DevTools from './DevTools';
 
 render(
   <Provider store={store}>
-    <div>
-      <h1>Inicjalizacja projektu</h1>
-      <DevTools />
-    </div>
+    <Router history={hashHistory} routes={routes}/>
   </Provider>,
   document.getElementById('root')
 );
